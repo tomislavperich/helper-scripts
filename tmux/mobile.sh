767b03afc22491b911b95e57c517ad21142660b6
Tomislav Peric <toca91@gmail.com>
2021-05-11 18:35:03 +0200

    Better device handling for mobile development script

--- tmux/mobile.sh
+++ tmux/mobile.sh
@@ -23,8 +23,9 @@ tmux send-keys -t "main" "npm run start" C-m
 # Start android
 tmux split-window -h -c $path
 tmux select-pane -t "$session:main.2"
-tmux send-keys 'emulator -noaudio -avd $(emulator -list-avds) > /dev/null & disown' C-m
+# tmux send-keys 'emulator -noaudio -avd $(emulator -list-avds) > /dev/null & disown' C-m
 sleep 3
-tmux send-keys 'npm run android' C-m
+
+tmux send-keys 'adb devices; npm run android; scrcpy > /dev/null & disown' C-m
 
 tmux -2 attach-session -t $session
