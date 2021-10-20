65e03a6ef1065e4622225ef553ceaf734fd33714
Tomislav Peric <toca91@gmail.com>
2021-10-20 23:28:31 +0200

    Change VPN client

--- utility/evening-dev.sh
+++ utility/evening-dev.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 . "$HOME/scripts/.env"
-tmux kill-session -t vpn
 tmux kill-session -t main
-echo "$USER_PASS" | sudo -S killall slack zoom openvpn
+echo "$USER_PASS" | sudo -S killall slack zoom openconnect
 pkill -9 viber
