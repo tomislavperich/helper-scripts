273d4846f78397a9c00eb5dbc62c34270146c27b
Tomislav Perić <toca91@gmail.com>
2023-03-01 15:55:02 +0100

    Play a confirmation sound when toggling audio devices

--- utility/toggle-audio-dev.sh
+++ utility/toggle-audio-dev.sh
@@ -4,4 +4,4 @@
 # not start with '*' (not selected), take first, extract ID
 # and use wpctl to set is as the active device
 alternate_output_id=$(wpctl status | grep -A3 "Sinks:" | grep -v '*' | grep -oP "\d{1,3}\.\s.*" | grep -oP '\d+\. ' | sed 's/\.\s//' | head -n1)
-wpctl set-default $alternate_output_id
+wpctl set-default $alternate_output_id && mpv --no-terminal /usr/share/sounds/freedesktop/stereo/window-attention.oga
